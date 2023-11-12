37bcbfd79c2fe55feca7b5e1a1843bc5a07c3787
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-11-12 17:33:42 +0900

    feature：職種マスタのAPIController作成

--- src/backend/app/Http/Controllers/JobCategoryController.php
+++ src/backend/app/Http/Controllers/JobCategoryController.php
@@ -4,55 +4,87 @@ namespace App\Http\Controllers;
 
 use App\Models\JobCategory;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
 
 class JobCategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * 職種マスタ一覧取得
      */
     public function index()
     {
-        $job_categories = JobCategory::all();
-        return response()->json($job_categories);
+        $jobCategories = JobCategory::all();
+        return response()->json($jobCategories);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * 職種マスタ取得
      */
-    public function create()
+    public function show($id)
     {
-        //
+        try {
+            $jobCategory = JobCategory::find($id);
+            if (!$jobCategory) {
+                throw new ModelNotFoundException();
+            }
+            return response()->json($jobCategory);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Job category not found'], 404);
+        }
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 職種マスタ登録
      */
-    public function store(Request $request)
+    public function create(Request $request)
     {
-        //
+        try {
+            $data = $request->validate([
+                'name' => 'required|string',
+                'permalink' => 'required|string',
+                'status' => 'required',
+            ]);
+            JobCategory::create($data);
+            return response()->json(['result' => 'ok']);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        }
     }
 
     /**
-     * Display the specified resource.
+     * 職種マスタ更新
      */
-    public function show(JobCategory $jobCategory)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $data = $request->validate([
+                'name' => 'required|string',
+                'permalink' => 'required|string',
+                'status' => 'required',
+            ]);
+            $jobCategory = JobCategory::findOrFail($id);
+            $jobCategory->update($data);
+            return response()->json(['result' => 'ok']);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        }
     }
 
     /**
-     * Update the specified resource in storage.
+     * 職種マスタ削除
      */
-    public function update(Request $request, JobCategory $jobCategory)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(JobCategory $jobCategory)
-    {
-        //
+        try {
+            $jobCategory = JobCategory::find($id);
+            if (!$jobCategory) {
+                throw new ModelNotFoundException();
+            }
+            $jobCategory->delete();
+            return response()->json(['result' => 'ok']);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Job category not found'], 404);
+        }
     }
 }
