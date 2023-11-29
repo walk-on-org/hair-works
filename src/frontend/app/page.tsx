a309e12486a36ae07a8defb02d6dd936bf3a04c8
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-11-29 12:01:01 +0900

    feature：不要な処理は削除

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,7 +1,3 @@
-import { apiServer } from "@/utils/api";
-
 export default async function Home() {
-  const data = await apiServer.get<any[]>("job_categories");
-  console.log(data);
   return <div>Hello World</div>;
 }
