ff8c1ebf1333182c9123e05135547436666b514b
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-12-02 19:11:32 +0900

    feature：職種マスタ画面に必要なコンポーネントを追加

--- src/frontend/components/hook-form/index.ts
+++ src/frontend/components/hook-form/index.ts
@@ -4,7 +4,7 @@ export * from "./rhf-checkbox";*/
 
 export { default } from "./form-provider";
 //export { default as RHFCode } from "./rhf-code";
-//export { default as RHFSwitch } from "./rhf-switch";
+export { default as RHFSwitch } from "./rhf-switch";
 //export { default as RHFEditor } from "./rhf-editor";
 //export { default as RHFSlider } from "./rhf-slider";
 export { default as RHFTextField } from "./rhf-text-field";
